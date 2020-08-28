7db969171922ac2e007b3099b71153b9c09b7e82
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2020-08-28 16:58:54 +0900

    Basic認証の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :masic_auth
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username,password|
+      username == 'admin' && password == '5555'  
+  end
 end
