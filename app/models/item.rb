df73c760118345e000ab5970c98e6d61a5b6d1c0
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2020-09-03 17:03:46 +0900

    金額非同期計算機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
 
   validates :img, :name, :message, :price, :category, :sales_status, :shipping_fee, :prefecture, :scheduled_delively, presence: true
   validates :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delively_id, numericality: {other_than: 0}
-  validates :price, numericality: {greater_than_or_equal_to: 300, less_rhan_or_equal_to: 9999999}
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_rhan_or_equal_to: 9999999, only_integer: true}
   PRICE_REGEX = /\A[0-9]+\z/.freeze
   validates_format_of :price, with: PRICE_REGEX, message: 'Half-width number'
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     Faker::Config.locale = :ja
     nickname { Faker::Name.name }
     email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
+    password { Faker::Internet.password(min_length: 6) }
     password { password }
     password_confirmation { password }
     family_name { Faker::Name.last_name }
