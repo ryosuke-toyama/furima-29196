0d7619e630047dfa0fdf1aa4fee12802ea58b92f
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2020-09-03 18:44:03 +0900

    itemモデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    Faker::Config.locale = :ja
+    name { Faker::Name.name }
+    message { Faker::Lorem.sentence}
+    price { 3000 }
+    category_id { 1 }
+    sales_status_id { 1 }
+    shipping_fee_id { 1 }
+    prefecture_id { 1 }
+    scheduled_delively_id { 1 }
+    # association :user
   end
 end
