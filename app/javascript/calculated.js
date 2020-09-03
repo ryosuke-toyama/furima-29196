56ecbc877b97e19e6424cbf35eff5ef79e506936
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2020-09-03 19:04:18 +0900

    console.log削除

--- app/javascript/calculated.js
+++ app/javascript/calculated.js
@@ -3,10 +3,7 @@ function calculation(){
   item_price.addEventListener('input', function() {
     const add_tax_form = document.getElementById("add-tax-price")
     const profit_form = document.getElementById("profit")
-
     let price = item_price.value
-    console.log(price)
-
     add_tax_form.innerHTML = Math.floor(price / 10);
     profit_form.innerHTML = Math.floor(price - add_tax_form.innerHTML);
   });
