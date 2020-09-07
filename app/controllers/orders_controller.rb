59dc0d07eaa0525b2c7eeeeed0280b742c4686e5
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2020-09-07 19:18:05 +0900

    カード決済機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,34 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order = Order.new(payment_price: order_params[:payment_price], user_id: order_params[:user_id], item_id: order_params[:item_id])
+    if @order.valid?
+      pay_item
+      @order.save
+      @item.update(sold: true)
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:token).merge(payment_price: @item.price).merge(user_id: current_user.id).merge(item_id: @item.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:payment_price],
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_09_05_032041) do
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "payment_price", null: false
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
