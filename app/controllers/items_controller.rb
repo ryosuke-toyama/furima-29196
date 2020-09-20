af23b5bffb587a65792f04acc53c6798d254e077
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2020-09-20 13:34:25 +0900

    編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = ItemTag.new(item_params)
+    @item = ItemTag.new(item_tag_params)
     @item.sold = false
     if @item.valid? 
       @item.save
@@ -31,8 +31,8 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
-    if @item.update(item_params)
-      redirect_to item_path(item)
+    if @item.valid?
+      redirect_to item_path(@item)
     else
       render :edit
     end
@@ -49,7 +49,10 @@ class ItemsController < ApplicationController
   end
   private
 
-  def item_params
+  def item_tag_params
     params.require(:item_tag).permit(:name, :message, :price, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delively_id, :tag_name, images: []).merge(user_id: current_user.id, item_id: params[:item_id], tag_id: params[:tag_id])
+  end
+  def item_params
+    params.require(:item).permit(:name, :message, :price, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delively_id, images: [])
   end
 end
