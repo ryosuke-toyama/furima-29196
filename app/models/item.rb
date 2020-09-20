bf8a0dec2c9da33764649f277ac0603650106662
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2020-09-20 14:45:04 +0900

    update時のバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :scheduled_delively
   has_many :item_tag_relations, dependent: :destroy
   has_many :tags, through: :item_tag_relations, dependent: :destroy
+
+  validates :images, :name, :message, :price, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delively_id, presence: true
+  validates :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delively_id, numericality: { other_than: 0, message: 'を選択してください' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
+  PRICE_REGEX = /\A[0-9]+\z/.freeze
+  validates_format_of :price, with: PRICE_REGEX, message: 'は半角数字で入力してください'
+
 end
