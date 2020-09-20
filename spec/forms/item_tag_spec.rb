4f2a91d3537ab4a18dafbbe3dbcdd44ffdea5260
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2020-09-20 12:45:11 +0900

    単体テスト日本語化

---
+++ spec/forms/item_tag_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe ItemTag, type: :model do
+  before do
+    @item = FactoryBot.build(:item_tag)
+    @item.images = fixture_file_upload('public/images/test_image.png')
+  end
+
+  describe '商品出品機能' do
+    it '画像がないと登録できない' do
+      @item.images = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("画像を入力してください")
+    end
+    it '商品名がないと登録できない' do
+      @item.name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("商品名を入力してください")
+    end
+    it '商品の説明がないと登録できない' do
+      @item.message = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("商品の説明を入力してください")
+    end
+    it 'カテゴリーの情報がないと登録できない' do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include('カテゴリーを選択してください')
+    end
+    it '商品の状態についての情報がないと登録できない' do
+      @item.sales_status_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include('商品の状態を選択してください')
+    end
+    it '配送料の負担についての情報がないと登録できない' do
+      @item.shipping_fee_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include('配送料の負担を選択してください')
+    end
+    it '発送元の地域についての情報がないと登録できない' do
+      @item.prefecture_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include('発送元の地域を選択してください')
+    end
+    it '発送までの日数についての情報がないと登録できない' do
+      @item.scheduled_delively_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include('発送までの日数を選択してください')
+    end
+    it '価格についての情報がないと登録できない' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("価格を入力してください")
+    end
+    it '価格の範囲が300より低いと登録できない' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include('価格は300以上の値にしてください')
+    end
+    it '価格の範囲が9999999より高いと登録できない' do
+      @item.price = 10_000_000
+      @item.valid?
+      expect(@item.errors.full_messages).to include('価格は9999999以下の値にしてください')
+    end
+    it '価格が全角数字だと登録できない' do
+      @item.price = '１０００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('価格は半角数字で入力してください')
+    end
+  end
+end
--- spec/forms/order_address_spec.rb
+++ spec/forms/order_address_spec.rb
@@ -15,42 +15,42 @@ RSpec.describe OrderAddress, type: :model do
     it '郵便番号がないと購入できない' do
       @order_address.postal_code = ''
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Postal codeを入力してください")
+      expect(@order_address.errors.full_messages).to include("郵便番号を入力してください")
     end
     it '郵便番号に-(ハイフン)がないと購入できない' do
       @order_address.postal_code = '1111111'
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Postal codeはハイフン(-)を含めた7桁で入力してください')
+      expect(@order_address.errors.full_messages).to include('郵便番号はハイフン(-)を含めた7桁で入力してください')
     end
     it '都道府県がないと購入できない' do
       @order_address.prefectures_id = 0
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Prefecturesを選択してください')
+      expect(@order_address.errors.full_messages).to include('都道府県を選択してください')
     end
     it '市区町村がないと購入できない' do
       @order_address.city = ''
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Cityを入力してください")
+      expect(@order_address.errors.full_messages).to include("市町村を入力してください")
     end
     it '番地がないと購入できない' do
       @order_address.address = ''
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Addressを入力してください")
+      expect(@order_address.errors.full_messages).to include("番地を入力してください")
     end
     it '電話番号がないと購入できない' do
       @order_address.phone_number = ''
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Phone numberを入力してください")
+      expect(@order_address.errors.full_messages).to include("電話番号を入力してください")
     end
     it '電話番号が12桁以上だと購入できない' do
       @order_address.phone_number = '111111111111'
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Phone numberは数字のみの11桁で入力してください')
+      expect(@order_address.errors.full_messages).to include('電話番号は数字のみの11桁で入力してください')
     end
     it 'トークンが発行されないと購入できない' do
       @order_address.token = ''
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Tokenを入力してください")
+      expect(@order_address.errors.full_messages).to include("クレジットカード情報を入力してください")
     end
   end
 end
